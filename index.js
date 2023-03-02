9a4d8ae5d48aef93cab90d721412f1b8946228e8
Adam <konber@gmail..com>
2023-03-02 08:24:52 -0700

    added some more react componets

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-ReactDOM.render(<h1>This Is React</h1>, document.getElementById("root"))
+function MainContent() {
+    return (
+        <h1 className="main-content">IM learning react</h1>
+     )
+}
+
+ReactDOM.render(
+    <div>
+        <MainContent />
+    </div>,
+    document.getElementById("root")
+)
+
+
+const h1 = document.createElement("p")
+h1.textContent = "im starting to get a hang of this, lots to learn though!"
+h1.className = "text"
+document.getElementById("root").append(h1)
+
+// JSX (JAVASCRIPT XML) 
+
+const element = <h1 className="text">This is JSX</h1>
+console.log(element)
+
+// ReactDOM.render(element, document.getElementById("root"))
 
